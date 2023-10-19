607e66d1e91cab1503a7a6fa2981e5753ae14d24
alyonastr <146359188+alyonastr@users.noreply.github.com>
2023-10-19 16:50:57 +0000

    jhjhhh

--- lab2_task5.py
+++ lab2_task5.py
@@ -1,5 +1,13 @@
 a=int(input('Введите число:'))
-b==int(input('Введите число:'))
-if a % b ==0:
-    print('Делится')
-else 
+b=int(input('Введите число:'))
+if b==0:
+    print('На ноль делить нельзя')
+else: 
+    d= a % b 
+    c= a//b
+    if d==0:
+     print('Делится')
+     print(c)
+    else:
+     print(d)
+     print(c)
