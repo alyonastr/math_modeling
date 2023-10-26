784897bc364bc1cbf8a8228555aab234b9d59f92
alyonastr <146359188+alyonastr@users.noreply.github.com>
2023-10-26 16:54:03 +0000

    jnjhnjhnjh

---
+++ lab3_task1.py
@@ -0,0 +1,5 @@
+gravity_constant=6.67*10**(-11)
+speed_of_light=3*(10**8)
+acceleration_of_gravity=9.8
+elementary_charge=1.602176634*(10**(-19))
+atm=101325
---
+++ lec3_import_as.py
@@ -0,0 +1,8 @@
+import lec3_my_module as lmm 
+
+print(lmm.a)
+
+b=lmm.b * 3
+print(b)
+
+print(lmm.c[2]+b+lmm.c[0])
