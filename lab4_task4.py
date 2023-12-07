0e3abe6a6bc7350da4e7fe9579f73bac5951c69b
alyonastr <146359188+alyonastr@users.noreply.github.com>
2023-12-07 16:37:42 +0000

    uhulj

--- lab4_task4.py
+++ lab4_task4.py
@@ -1,12 +1,9 @@
 import numpy as np 
 a=10
 b=30
-c=[]
-def y(x):
-    for i in range (a,b):
-        y=x**2
-        c.append(y)
-    return c
+def y(a, b, N):
+    x=np.linspace(a, b, N)
+    yn=x**2
+    return yn
 
-res=y(5)
-print(res)
+print(y(3,5,2))
--- lab4_task5.py
+++ lab4_task5.py
@@ -1,15 +1,12 @@
 import numpy as np 
-figure=input('Введите тип фигуры:')
-a=input('Введите сторону/радиус')
-b=input('Введите сторону/высоту')
 
-def func(figure, a, b):
+def square(figure, * args):
     if figure == 'круг':
-        pl=3.14*(a**2)
+        pl=3.14*(args[0]**2)
     if figure == 'квадрат':
-        pl=a*b
+        pl=args[0]*args[1]
     if figure == 'треугольник':
-        pl=(a*b)/2
+        pl=(args[0]*args[1])/2
     return pl
 
-print(func(figure, a, b))
+print(square('круг', 6, 9))
