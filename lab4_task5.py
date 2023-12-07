81f0b146d92774b2a8b25cc89c140f1784529a20
alyonastr <146359188+alyonastr@users.noreply.github.com>
2023-12-07 12:28:18 +0000

    лддлждл

--- lab4_task5.py
+++ lab4_task5.py
@@ -1,9 +1,15 @@
 import numpy as np 
-def mult_func(array):
-    s=0
-    for i in range(len(array)):
-        s += array[i]
-    return s/len(array)
+figure=input('Введите тип фигуры:')
+a=input('Введите сторону/радиус')
+b=input('Введите сторону/высоту')
 
-test = np.arange(0,100,1)
-print(mult_func(test))
+def func(figure, a, b):
+    if figure == 'круг':
+        pl=3.14*(a**2)
+    if figure == 'квадрат':
+        pl=a*b
+    if figure == 'треугольник':
+        pl=(a*b)/2
+    return pl
+
+print(func(figure, a, b))
