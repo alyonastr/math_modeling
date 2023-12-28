619707f1e55c6f18d1e48f69cdae6241eb82de7f
alyonastr <146359188+alyonastr@users.noreply.github.com>
2023-12-28 17:02:13 +0000

    kufyuj

--- lab6_task4.py
+++ lab6_task4.py
@@ -1,14 +1,20 @@
 import matplotlib.pyplot as plt  
 import numpy as np 
 
-x = [0, r*np.cos(fi), r*np.cos(fi),  r*np.cos(fi), 0] 
-y = [0, 0, r*np.sin(fi), 0] 
- 
-plt.plot(y, x, color='black', label='Graf 1', marker='>', ms=5) 
-plt.plot(x, y, color='black', label='Graf 2', marker='o', ms=3) 
- 
-plt.xlabel('Coord: x') 
-plt.ylabel('Coord: y') 
-plt.title('Base') 
-plt.axis('equal') 
-plt.savefig('fig_8.png')
+def circle_plotter(b=10, k=4, ):
+    
+    x = np.arange(-2*r, 2*r, 0.1)
+    y = np.arange(-2*r, 2*r, 0.1)
+
+   
+    X, Y = np.meshgrid(x, y)
+
+    
+
+    plt.contour(X, Y, fxy, levels=[0])
+    plt.axis('equal')
+    
+    plt.savefig('fig_4.png')
+    
+if __name__ == '__main__':
+	circle_plotter()
