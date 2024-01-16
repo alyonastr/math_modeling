b2d448d3b776088773428017ea238921e30ac5ea
alyonastr <146359188+alyonastr@users.noreply.github.com>
2024-01-16 19:55:06 +0000

    jnbhibh

--- lab7_task2.py
+++ lab7_task2.py
@@ -4,16 +4,19 @@ from matplotlib.animation import FuncAnimation
 
 def circle_move(R, t):
     fi = np.arange(0, 2*np.pi, 0.1)
-    a = np.arange(0, 20, 0.01)
-    R = a*t(fi)
+    t = np.arange(0, 20, 0.01)
+    a = np.arange(0, 5, 1)
+    R = a*t
+    x =R*np.cos(fi)
+    y =R*np.sin(fi)
     return R
 
 def animate(i):
-    ball.set_data(circle_move(R=0.5, vx0=0.01, vy0=0.01, time=i))
+    ball.set_data(circle_move(R,t=i))
 
 if __name__=='__main__':
     fig, ax = plt.subplots()
-    ball, = plt.plot([], [], '-', color='y', label='Ball')
+    ball, = plt.plot([], [], 'o', color='y', label='Ball')
 
     edge=3
     plt.axis('equal')
@@ -25,4 +28,4 @@ if __name__=='__main__':
                         frames=180,
                         interval=30)
 
-ani.save('animation_3.gif')
+ani.save('animation_4.gif')
