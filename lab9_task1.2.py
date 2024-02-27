efe28667609dbe24fae0d5145f4c587c521763b6
alyonastr <146359188+alyonastr@users.noreply.github.com>
2024-02-27 16:42:57 +0000

    yuugkui

--- lab9_task1.2.py
+++ lab9_task1.2.py
@@ -2,22 +2,25 @@ import numpy as np
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 
-r = np.arange(6471, 6371, 1)
+r = np.arange(6471000, 637100000, 100)
 
 def met(V, r):
-    dVdr = (G * M)/ V* r**2
+    dVdr = (G * M)/ (V * r**2)
     return dVdr
 
-V_0 = 120
+V_0 = 0.1
 M = 5.9736*10**24
-G = 6.6743 * 10 **-11
+G = 6.6743 * 10 **(-11)
 
 
 V_r = odeint(met, V_0, r)
+print(V_r)
  
 plt.plot(r, V_r[:,0], label='')
 plt.xlabel('')
 plt.ylabel('')
+plt.xlim(0, 637100000)
+plt.ylim(0, 637100000)
 plt.title('')
 plt.legend()
 
--- lab9_task2.2.py
+++ lab9_task2.2.py
@@ -5,11 +5,11 @@ import matplotlib.pyplot as plt
 t = np.arange(0, 500, 0.1)
 
 def bact(S, t):
-    dSdt = k * np.sqrt(S / np.pi) * E * S
+    dSdt = k * np.sqrt(S / np.pi) * E * S * np.cos((np.pi/12)*(t - 12))
     return dSdt
 
 S_0 = 1600
-k = 5 * 10
+k = 340 * 10**(-8)
 E = 1360
 
 S_t = odeint(bact, S_0, t)
